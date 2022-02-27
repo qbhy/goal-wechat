8ca10c0590849e4a44c3dc19398527774f4cfc4a
qbhy <96qbhy@gmail.com>
2022-02-27 12:52:25 +0800

    feat: 完善服务提供者和readme

--- service_provider.go
+++ service_provider.go
@@ -2,14 +2,34 @@ package wechat
 
 import (
 	"github.com/goal-web/contracts"
+	"github.com/silenceper/wechat/v2/miniprogram"
+	"github.com/silenceper/wechat/v2/officialaccount"
+	"github.com/silenceper/wechat/v2/openplatform"
+	"github.com/silenceper/wechat/v2/pay"
+	"github.com/silenceper/wechat/v2/work"
 )
 
 type ServiceProvider struct {
 }
 
 func (s ServiceProvider) Register(app contracts.Application) {
-	app.Singleton("wechat", func() {
-
+	app.Singleton("wechat", func(config contracts.Config) Factory {
+		return NewFactory(config.Get("wechat").(*Config))
+	})
+	app.Singleton("wechat.oc", func(wechat Factory) *officialaccount.OfficialAccount {
+		return wechat.OfficialAccount()
+	})
+	app.Singleton("wechat.pay", func(wechat Factory) *pay.Pay {
+		return wechat.Payment()
+	})
+	app.Singleton("wechat.op", func(wechat Factory) *openplatform.OpenPlatform {
+		return wechat.OpenPlatform()
+	})
+	app.Singleton("wechat.mini", func(wechat Factory) *miniprogram.MiniProgram {
+		return wechat.MiniProgram()
+	})
+	app.Singleton("wechat.work", func(wechat Factory) *work.Work {
+		return wechat.Work()
 	})
 }
 
